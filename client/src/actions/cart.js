afc9d40917bc4a70fd93b04a293fccfbfe2a4184
karishmas6 <carishmashukla28@gmail.com>
2021-05-31 01:25:06 +0530

    Remove from cart action

--- client/src/actions/cart.js
+++ client/src/actions/cart.js
@@ -22,8 +22,14 @@ export const addToCart = (id,qty) => async(dispatch,getState) => {
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems)) 
 }
 
-
-
-
-
 // getState lets you get any part of the state. Similar to useSelector 
+
+export const removeFromCart = (id) => async(dispatch, getState) => {
+
+    dispatch({
+        type: CART_REMOVE_ITEM,
+        payload: id
+    })
+
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems)) 
+}
