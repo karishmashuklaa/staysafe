3b4e231030d0079a738ba16d7e6473205834a660
karishmas6 <carishmashukla28@gmail.com>
2021-06-17 19:39:03 +0530

    frontend auth

--- client/src/store.js
+++ client/src/store.js
@@ -4,7 +4,7 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
-
+const userLoginFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
 const initialState = {
     cart: { cartItems: cartItemsFromStorage}
 };
