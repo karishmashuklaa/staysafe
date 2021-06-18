a2a6f94dcb1befb7920cd5f1426400b99d2b5cf8
karishmas6 <carishmashukla28@gmail.com>
2021-06-18 18:52:58 +0530

    frontend auth

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,15 +1,33 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import '../../App.css'
 import { Form, Row, Col, Button } from 'react-bootstrap'
 import {useSelector, useDispatch} from 'react-redux'
+import {Link} from 'react-router-dom'
 import FormContainer from '../layout/FormContainer'
+import Message from '../layout/Message'
+import Loader from '../layout/Loader'
 import {login} from '../../actions/user'
 
-const Login = () => {
+const Login = ({location, history}) => {
     const DEFAULT_DATA = {
         email: '',
         password: ''
     }
     const [form, setForm] = useState(DEFAULT_DATA)
+
+    const redirect = location.search ? location.search.split('=')[1] : '/'
+
+    const userLogin = useSelector(state => state.userLogin)
+    const {loading, userInfo, error} = userLogin
+
+    const dispatch = useDispatch()
+
+    // if user is already logged in
+    useEffect(() => {
+        if(userInfo) {
+            history.push(redirect)
+        }
+    }, [history, userInfo, redirect])
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -21,12 +39,14 @@ const Login = () => {
 
     const submitForm = (e) => {
        e.preventDefault()
-       console.log('Submitted')
+       dispatch(login(form.email,form.password))
     }
 
     return (
         <FormContainer>
             <h1> Sign In </h1>
+            {error && <Message variant='danger'>{error}</Message>}
+            {loading && <Loader />}
             <Form>
                 <Form.Group controlId="email">
                     <Form.Label>Email Address </Form.Label>
@@ -55,8 +75,19 @@ const Login = () => {
             type='submit' 
             variant='primary' 
             onClick={submitForm}> 
-                Sign In 
+                Sign In
             </Button>
+
+            <Row className='py-3'>
+                <Col>
+                Don't have an account?
+                <Link 
+                className='link'
+                to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+                    {''} Register
+                </Link>
+                </Col>
+            </Row>
         </FormContainer>
     )
 }
