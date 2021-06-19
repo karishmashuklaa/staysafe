11f9cd5acbf66a7113f87c6883070af5c92b3434
karishmas6 <carishmashukla28@gmail.com>
2021-06-19 19:51:09 +0530

    profile

--- client/src/reducers/user.js
+++ client/src/reducers/user.js
@@ -3,9 +3,14 @@ import {
     USER_LOGIN_SUCCESS,
     USER_LOGIN_FAIL,
     USER_LOGOUT,
+
     USER_REGISTER_REQUEST,
     USER_REGISTER_SUCCESS,
-    USER_REGISTER_FAIL
+    USER_REGISTER_FAIL,
+
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_DETAILS_FAIL
 } from '../constants/userConstants'
 
 export const userLoginReducer = (state = {}, action) => {
@@ -49,6 +54,31 @@ export const userRegisterReducer = (state = {}, action) => {
                 userInfo: payload
             }
         case USER_REGISTER_FAIL:
+            return {
+                loading: false,
+                error: payload
+            }
+    
+        default:
+            return state
+    }
+
+}
+
+export const userDetailsReducer = (state = {user: {}}, action) => {
+    const{ type,payload } = action
+
+    switch(type){
+        case USER_DETAILS_REQUEST:
+            return {
+                loading: true
+            }
+        case USER_DETAILS_SUCCESS:
+            return {
+                loading: false,
+                user: payload
+            }
+        case USER_DETAILS_FAIL:
             return {
                 loading: false,
                 error: payload
