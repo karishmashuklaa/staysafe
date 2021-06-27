2e8420f9e7e08c59c659320ada47c034842dafb0
karishmas6 <carishmashukla28@gmail.com>
2021-06-27 19:32:35 +0530

    header

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../../App.css'
 import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import {useSelector, useDispatch} from 'react-redux'
