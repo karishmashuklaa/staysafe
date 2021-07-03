5be2b31c150ea153be63f3f92db7891ee0fb7ef7
karishmas6 <carishmashukla28@gmail.com>
2021-07-03 21:44:40 +0530

    payment

---
+++ client/src/components/cart/Payment.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Col,Form,Button } from 'react-bootstrap'
+import FormContainer from '../layout/FormContainer'
+import { useSelector,useDispatch } from 'react-redux'
+import CheckoutSteps from './CheckoutSteps'
+
+
+const Payment = ({ history }) => {
+
+    const cart = useSelector(state => state.cart)
+    const { shippingAddress } = cart
+
+    const dispatch = useDispatch()
+    const [paymentMethod, setPaymentMethod] = useState('PayPal')
+
+    if(!shippingAddress.address) {
+        history.push("/shipping")
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        // dispatch(savePaymentMethod(setPaymentMethod))
+        history.push("/placeholder")
+    }
+
+    return (
+        <FormContainer>
+            <CheckoutSteps step1 step2 step3 />
+            <Form onSubmit={submitHandler}>
+                <Form.Group>
+                    <Form.Label as="legend">
+                        Select Method
+                        <Col>
+                        <Form.Check
+                        type="radio"
+                        label="PayPal or Credit Card"
+                        id="paypal"
+                        name="paymentMethod"
+                        checked
+                        
+                        />
+                        </Col>
+                    </Form.Label>
+                </Form.Group>
+                <Button type="submit" variant="primary">
+                    Continue
+                </Button>
+            </Form>
+        </FormContainer>
+    )
+}
+
+export default Payment
